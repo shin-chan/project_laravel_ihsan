47873d3f99adde25a65ce84353dc12aceaffbc4e
shin-chan <ihsanromdhoni2182@gmail.com>
2022-04-29 17:06:20 +0700

    penambahan route web

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::prefix('admin')->group(function () {
 
     Route::get('/jsIntroduction', 'JsintroController@index')->name('jsintro');
     Route::get('/jsIntroduction_next', 'JsintroController@index_next')->name('jsintro_next');
+
+    Route::get('/jsIhsan', 'IhsanController@index')->name('jsihsan');
+    Route::get('/jsIhsan_next', 'IhsanController@index_next')->name('jsihsan_next');
   
 
 });
